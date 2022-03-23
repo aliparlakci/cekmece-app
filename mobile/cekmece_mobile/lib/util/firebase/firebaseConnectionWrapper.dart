5bd275e369a9bd9fcdf8077d85b1b9da8ce0e718
Ahmet Ömer Kayabaşı <69629296+amedumer@users.noreply.github.com>
2022-03-23 19:16:52 +0300

    Firebase connection builder created. Application package was renamed.

---
+++ mobile/cekmece_mobile/lib/util/firebase/firebaseConnectionWrapper.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+//Checks firebase connection. If established, forwards to main application. Displays a loading spinner otherwise.
+class FirebaseWrapper extends StatefulWidget {
+  @override
+  _FirebaseWrapperState createState() => _FirebaseWrapperState();
+}
+
+class _FirebaseWrapperState extends State<FirebaseWrapper> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print('Cannot connect to firebase: ' + snapshot.error.toString());
+            return MaterialApp(
+              home: Text("Cannot connect to firebase"),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            print('Firebase connected');
+            return Container();
+          }
+
+          return MaterialApp(
+            home: Text("Loading"),
+          );
+        });
+  }
+}
