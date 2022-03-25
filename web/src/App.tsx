31537403d768b5a71c733270bb2bae2a68b1f66f
Ali Parlakci <aliparlakci@sabanciuniv.edu>
2022-03-25 12:27:20 +0300

    Paperbase example added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,14 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
+import Paperbase from './Paperbase'
+
 import "./App.css"
 
 function App() {
     return (
         <>
             <Router>
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/users">Users</Link>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
                     <Switch>
                         <Route path="/about">
                             <About />
@@ -30,11 +16,13 @@ function App() {
                         <Route path="/users">
                             <Users />
                         </Route>
+                        <Route path="/paperbase">
+                            <Paperbase />
+                        </Route>
                         <Route path="/">
                             <Home />
                         </Route>
                     </Switch>
-                </div>
             </Router>
         </>
     )
