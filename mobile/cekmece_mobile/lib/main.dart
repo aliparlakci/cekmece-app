7ec66bda72e2986aa4e15da4c835f43e104fb29d
Ahmet Ömer Kayabaşı <69629296+amedumer@users.noreply.github.com>
2022-05-02 08:18:36 +0300

    Network class created

--- mobile/cekmece_mobile/lib/main.dart
+++ mobile/cekmece_mobile/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:cekmece_mobile/util/blocProviders.dart';
+import 'package:cekmece_mobile/util/network/networkProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(BlocProviders());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<NetworkService>(create: (_) => NetworkService())
+    ],
+    child: BlocProviders(),
+  ));
 }
