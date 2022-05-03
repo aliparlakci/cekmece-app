97c963e1d2e2f5ebef883bd27354de48def4e8e3
Ahmet Ömer Kayabaşı <69629296+amedumer@users.noreply.github.com>
2022-05-04 00:18:34 +0300

    out of stock error shown on mobile

--- mobile/cekmece_mobile/lib/views/homeView/homeView.dart
+++ mobile/cekmece_mobile/lib/views/homeView/homeView.dart
@@ -29,48 +29,7 @@ class _HomeViewState extends State<HomeView> {
   NumberFormat numberFormat =
       NumberFormat.simpleCurrency(locale: "en-US", decimalDigits: 0);
   String clientURL = dotenv.env['CLIENT_URL']!;
-  List<Product> cars = [
-    const Product(
-        id: 1,
-        name: "RB-18B",
-        price: 49900,
-        number: 1,
-        model: 2019,
-        quantity: 1,
-        warranty: 1,
-        distributor: {"name": "RedBull"},
-        category: {"name": "Formula 2"}),
-    const Product(
-        id: 1,
-        name: "F1-75",
-        price: 85000,
-        number: 1,
-        model: 2019,
-        quantity: 1,
-        warranty: 1,
-        distributor: {"name": "Ferrari"},
-        category: {"name": "Formula 1"}),
-    const Product(
-        id: 1,
-        name: "W13",
-        price: 49900,
-        number: 1,
-        model: 2019,
-        quantity: 1,
-        warranty: 1,
-        distributor: {"name": "Mercedes"},
-        category: {"name": "Formula 3"}),
-    const Product(
-        id: 1,
-        name: "Guila",
-        price: 38750,
-        number: 1,
-        model: 2021,
-        quantity: 1,
-        warranty: 1,
-        distributor: {"name": "Alfa Romeo"},
-        category: {"name": "Sedan"})
-  ];
+  List<Product> cars = [];
   List<Map<String, dynamic>> distributors = [];
 
   void getCars() async {
