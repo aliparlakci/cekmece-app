f669c007d28279e0db4320083681a20ca91da871
JEREN ANNAGURBANOVA <jerena@sabanciuniv.edu>
2022-05-06 02:22:56 +0300

    updated html

--- backend/src/routes/invoiceRouter.ts
+++ backend/src/routes/invoiceRouter.ts
@@ -11,62 +11,49 @@ import UserService from "../services/userService"
 import CartService from "../services/cartService"
 import CarService from "../services/carService"
 import CategoryService from "../services/categoryService"
-//import { MockData } from "../mockData"
+var invoiceNum = 0
+
 
 
 const options = {
     formate: 'A4',
     orientation: 'portrait',
-    border: '2mm',
+    border: '0mm',
     header: {
         height: '15mm',
-        contents: '<h4 style=" color: red;font-size:20;font-weight:800;text-align:center;">CUSTOMER INVOICE</h4>'
+        
     },
     footer: {
-        height: '20mm',
-        contents: {
-            first: 'Cover page',
-            2: 'Second page',
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', 
-            last: 'Last Page'
-        }
     }
 }
 const mockdata = [
     {
-        name: "Product 1",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ullam repudiandae provident, deleniti ratione ipsum sunt porro deserunt",
-        unit:"pack",
-        quantity: 2,
-        price: 20,
-        imgurl: "https://micro-cdn.sumo.com/image-resize/sumo-convert?uri=https://media.sumo.com/storyimages/ef624259-6815-44e2-b905-580f927bd608&hash=aa79d9187ddde664f8b3060254f1a5d57655a3340145e011b5b5ad697addb9c0&format=webp"
+        item: "Product 1",
+        description: " Laisjf aksdjf akjfd",
+        unitCost: 2300,
+        quantity: 2
     },
     {
-        name: "Product 2",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ullam repudiandae provident, deleniti ratione ipsum sunt porro deserunt",
-        unit:"pack",
-        quantity: 4,
-        price: 80,
-        imgurl: "https://micro-cdn.sumo.com/image-resize/sumo-convert?uri=https://media.sumo.com/storyimages/ef624259-6815-44e2-b905-580f927bd608&hash=aa79d9187ddde664f8b3060254f1a5d57655a3340145e011b5b5ad697addb9c0&format=webp"
+        item: "Product 2",
+        description: " Laisjf aksdjf akjfd",
+        unitCost: 130000,
+        quantity: 1
     },
     {
-        name: "Product 3",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ullam repudiandae provident, deleniti ratione ipsum sunt porro deserunt",
-        unit:"pack",
-        quantity: 3,
-        price: 60,
-        imgurl: "https://micro-cdn.sumo.com/image-resize/sumo-convert?uri=https://media.sumo.com/storyimages/ef624259-6815-44e2-b905-580f927bd608&hash=aa79d9187ddde664f8b3060254f1a5d57655a3340145e011b5b5ad697addb9c0&format=webp"
-    },
+        item: "Product 3",
+        description: " Laisjf aksdjf akjfd",
+        unitCost: 73483,
+        quantity: 6
+    }
+    
 ]
 
 interface invoicee {
-    name: any;
+    item: any,
     description: any;
-    unit: any;
+    unitCost: any;
     quantity: any;
-    price: any;
-    total: number;
-    imgurl: any;
+    lineTotal: any;
   }
    
 
@@ -74,37 +61,40 @@ interface invoicee {
 function generatePdf() {
     return async function (req, res, next) {
         // i will not parse anything yet
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
         const html = fs.readFileSync(path.join(__dirname, '../invoiceHtml.html'), 'utf-8')
-        // may be we can add here userid or cartid
-        const filename = Math.random()+'_doc'+'.pdf';
+        invoiceNum = invoiceNum+1
+        const filename = invoiceNum+'purchaserId_'+'_doc'+'.pdf';
         const arrayy: invoicee [] = []
+        
 
 
         mockdata.forEach( d => {
             const prod: invoicee = {
-                name: d.name,
+                item: d.item,
                 description: d.description,
-                unit: d.unit,
+                unitCost: d.unitCost,
                 quantity: d.quantity,
-                price: d.price,
-                total: d.quantity * d.price,
-                imgurl: d.imgurl
+                lineTotal: d.quantity * d.unitCost,     
             }
             arrayy.push(prod);
         });
 
         let subtotal = 0;
         arrayy.forEach(i => {
-            subtotal += i.total
+            subtotal += i.lineTotal
         });
 
-        const tax = (subtotal * 20) / 100;
-        const grandtotal = subtotal - tax;
         const obj = {
             prodlist: arrayy,
             subtotal: subtotal,
-            tax: tax,
-            gtotal: grandtotal
+            invoiceNumber: invoiceNum,
+            name: 'Jeren',
+            surname: 'Annagurbanova',
+            address: 'Sabanci Universitesi Caddesi',
+            region: 'Istanbul, Turkey',
+            email: 'jerena@sabanciuniv.edu'
         }
 
         const document = {
@@ -112,7 +102,7 @@ function generatePdf() {
             data: {
                 products: obj
             },
-            path: './docs/' + filename
+            path: './src/docs/' + filename
         }
 
         pdf.create(document, options)
@@ -121,14 +111,8 @@ function generatePdf() {
             }).catch(error => {
                 console.log(error);
             });
-           const filepath = 'http://localhost:5001/invoice/download/' + filename;
 
-            // res.render('download', {
-            //       path: filepath
-            // });
-           
-            //res.sendFile(path.join(__dirname, '../invoiceHtml.html'));
-              
+            
 
 
     }
